45d71cee40556522486f718dbefcddad3e5da35c
DrKerbal <narenkasinath@outlook.com>
2016-11-06 13:24:55 -0500

    Update net_test.py

--- PiCamera Tests/net_test.py
+++ PiCamera Tests/net_test.py
@@ -1,10 +1,11 @@
 from networktables import *
 import time as t
 
+NetworkTable.setIPAddress("10.5.40.2")
 NetworkTable.setClientMode
-
-
+print NetworkTable.port
 ct = NetworkTable.getTable("cameratrack")
+
 center = 1
 
 while True:
